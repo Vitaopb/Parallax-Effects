d12a24efc31ffd2b7d1e8fbc5285bb3cfaa76fe1
Victor Lieuthier <victor.lieuthier@gmail.com>
2021-07-13 12:58:29 -0300

    1.0

---
+++ JS/scripts.js
@@ -0,0 +1,37 @@
+let moon = document.getElementById('moon')
+let stars = document.getElementById('stars')
+let mountains_behind = document.getElementById('mountains_behind')
+let text = document.getElementById('text')
+let btn_explore = document.getElementById('btn-explore')
+let mountains_front = document.getElementById('mountains_front');
+let header = document.querySelector('header');
+
+    
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+    stars.style.left = value * 0.25 + 'px';
+    moon.style.top = value * 1.05 + 'px';
+    mountains_behind.style.top = value * 0.5 + 'px';
+    mountains_front.style.top = value * 0 + 'px';
+    text.style.marginRight = value * 3 + 'px';
+    text.style.marginTop = value * 1.5 + 'px';
+    btn_explore.style.marginTop = value * 1.5 + 'px';
+    header.style.bottom = value * 0.5 + 'px';
+
+    if (value >= 450) {
+        text.style.display = "none"
+    }else {
+        text.style.display = 'block'
+    }
+    if (value >= 390) {
+        btn_explore.style.display = "none"
+
+    }else {
+        btn_explore.style.display = 'inline-block'
+    }
+})
+
+function click() {
+    
+}
+
